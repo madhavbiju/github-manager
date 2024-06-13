cfb9aa134e2478ec9adcac5aad8277b63b422bcd
madhav-biju <madhav.b@experionglobal.com>
2024-06-13 10:31:44 +0530

    Add a loader to Roles Page

--- src/pages/RepoRoles/RepoRolesHandler.tsx
+++ src/pages/RepoRoles/RepoRolesHandler.tsx
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 import { fetchCollaborators } from "./api/fetchRoles";
 import { Params, useParams } from "react-router-dom";
 import RepoRoles from "./RepoRoles";
+import { Loader } from "@mantine/core";
+import classes from "./RepoRoles.module.css";
 
 const RepoRolesHandler = () => {
   const { owner, repo } = useParams<Params>();
   const [collaborators, setCollaborators] = useState<Collaborator[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const data = await fetchCollaborators(owner!, repo!);
         setCollaborators(data.data);
       } catch (error) {
         // Handle error
       } finally {
+        setLoading(false);
       }
     };
 
@@ -24,6 +29,14 @@ const RepoRolesHandler = () => {
       // Cleanup code if needed
     };
   }, [repo, owner]);
+
+  if (loading) {
+    return (
+      <div className={classes.container}>
+        <Loader size={30} />
+      </div>
+    );
+  }
 
   return (
     <RepoRoles collaborators={collaborators} owner={owner!} repo={repo!} />
