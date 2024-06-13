36dabc888edc5bc2121d205ac8c096d70b760cad
madhav-biju <madhav.b@experionglobal.com>
2024-06-13 10:09:08 +0530

    Change Pic Url

--- src/pages/ChooseRepo/ChooseRepo.tsx
+++ src/pages/ChooseRepo/ChooseRepo.tsx
@@ -4,13 +4,7 @@ import { Image, Title } from "@mantine/core";
 const ChooseRepo = () => {
   return (
     <div className={classes.container}>
-      <Image
-        radius="xl"
-        h={200}
-        w="auto"
-        fit="contain"
-        src="/public/norepo.png"
-      />
+      <Image radius="xl" h={200} w="auto" fit="contain" src="/norepo.png" />
       <Title order={2} className={classes.title} ta="center" mt="md" mb={30}>
         Choose a repo to view its collaborators
       </Title>
